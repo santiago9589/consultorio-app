49be9528f1ae439b15e5de932f58bf87e44f0fe8
jesus <js@gmail.com>
2023-04-06 10:47:57 -0300

    nav responsive

--- src/main.tsx
+++ src/main.tsx
@@ -11,15 +11,15 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <main className=' shadow-xl shadow-blue-100  bg-[url(src/assets/26358.jpg)] bg-no-repeat  bg-left lg:bg-center   bg-cover grid grid-cols-5 grid-rows-5 lg:grid-cols-6 lg:grid-rows-6 
      w-full h-screen container mx-auto'>
-      <nav className="col-span-1 row-span-4 lg:col-span-6 lg:row-span-1 flex justify-between items-center">
-        <h1 className='p-2 text-2xl'>MEDICAL CLINICAL GROUP S.A</h1>
-        <section className='flex space-x-12 '>
-          <button  className='border-2 border-blue-100 rounded-lg p-2 uppercase bg-blue-200'>servicios</button>
-          <button  className='border-2 border-blue-100 rounded-lg p-2 uppercase bg-blue-200'>quienes somos</button>
-          <button  className='border-2 border-blue-100 rounded-lg p-2 uppercase bg-blue-200'>contactos</button>
-          <button  className='border-2 border-blue-100 rounded-lg p-2 uppercase bg-blue-200'>profesionales</button>
+      <nav className="col-span-1 row-span-4 lg:col-span-6 lg:row-span-1 flex justify-between items-center flex-col lg:flex-row">
+        <h1 className='p-2 lg:text-2xl md:text-xl w-full text-justify lg:w-fit'>MEDICAL CLINICAL GROUP S.A</h1>
+        <section className='flex lg:space-x-12 space-y-4 lg:space-y-0 p-2 box-border flex-col lg:flex-row'>
+          <button  className='border-2 border-blue-100 rounded-lg p-2 capitalize bg-blue-200 lg:text-lg md:text-xs' >servicios</button>
+          <button  className='border-2 border-blue-100 rounded-lg p-2 capitalize bg-blue-200 lg:text-lg md:text-xs'>quienes somos</button>
+          <button  className='border-2 border-blue-100 rounded-lg p-2 capitalize bg-blue-200 lg:text-lg md:text-xs'>contactos</button>
+          <button  className='border-2 border-blue-100 rounded-lg p-2 capitalize bg-blue-200 lg:text-lg md:text-xs'>profesionales</button>
         </section>
-        <img src={logo} alt="logo" className='w-40 h-full' />
+        <img src={logo} alt="logo" className='lg:w-40 lg:h-full w-full h-32 lg:block' />
       </nav>
       <RouterProvider router={router} />
       <footer className="border-2 border-red-500 row-start-5 col-span-5 lg:row-start-6 lg:col-span-6">
