7fb5986fdcb1614b4447cc7d16184ca084ffc036
jesus <js@gmail.com>
2023-04-18 09:07:47 -0300

    quienessomos ui update

--- src/quienessomos.tsx
+++ src/quienessomos.tsx
@@ -35,11 +35,11 @@ const WhoAreWePage = () => {
   }]
 
   return (
-    <section className='shadow-lg  p-2 gap-2 col-start-2 col-span-4 row-span-4  lg:col-span-6   justify-between lg:row-span-4 flex  items-start flex-wrap overflow-y-auto'>
+    <section className='shadow-lg  p-2 gap-4 col-start-2 col-span-4 row-span-4  lg:col-span-6   justify-between lg:row-span-4 flex  items-start flex-wrap overflow-y-auto'>
 
       {arrayInfo.map((info) => {
         return (
-          <section className='w-80  rounded-lg shadow-lg h-full flex flex-col justify-between items-center p-2'>
+          <section className='w-3/4 mx-auto rounded-lg shadow-lg h-full flex flex-col justify-between items-center p-2'>
             <h2 className='text-2xl font-bold capitalize'>{info.title}</h2>
             <p className='text-justify'>{info.content}</p>
             <section className='w-full h-1/3'>
